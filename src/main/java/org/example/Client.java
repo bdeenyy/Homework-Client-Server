32d31916c91521175cddb0362faaed9e7b8f2700
bdeenyy <109365710+bdeenyy@users.noreply.github.com>
2022-10-13 13:11:03 +0300

    Task complete (fix)

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String host = "localhost";
         int port = 8089;
 
